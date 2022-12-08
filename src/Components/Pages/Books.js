a2083a7afc170cb67b065f6ea743867a5322fb32
SimonGideon <simongideon918@gmail.com>
2022-12-08 20:34:50 +0300

    ADD routing

---
+++ src/Components/Pages/Books.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Books extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Books</h1>
+      </div>
+    );
+  }
+}
+
+export default Books;
---
+++ src/Components/Pages/Cartegory.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Cartegory = () => (
+  <div>
+    <h1>Cartegory</h1>
+  </div>
+);
+
+export default Cartegory;
