f11ad00e2f088afd2bb5d187aa0313b620336d96
SimonGideon <simongideon918@gmail.com>
2022-12-20 01:35:28 +0300

    ADD reducers

---
+++ src/redux/books/books.js
@@ -0,0 +1,31 @@
+const ADD_BOOK = 'Bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'Bookstore/books/REMOVE_BOOK';
+
+const initialState = [];
+
+// Actions
+
+export const addBook = (book) => ({
+  type: ADD_BOOK,
+  payload: book,
+});
+
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  payload: id,
+});
+
+// Reducer
+
+const bookReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+export default bookReducer;
