4822d463f89873afb14aa3f9820e294d23454115
SimonGideon <simongideon918@gmail.com>
2022-12-20 23:38:06 +0300

    Add Redux components

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/books';
 
-const Book = ({ id, title, author }) => {
-  const dispatch = useDispatch();
-  return (
-    <div>
-      <h2>{title}</h2>
-      <h3>{author}</h3>
-      <button type="button" onClick={() => dispatch(removeBook(id))}>Remove</button>
-    </div>
-  );
-};
+const Book = ({ title, author }) => (
+  <div>
+    <h2>{title}</h2>
+    <h3>{author}</h3>
+  </div>
+);
 
 Book.propTypes = {
-  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
 };
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 import Book from './Book';
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
   return (
     <div>
       <ul>
         {books.map((book) => (
           <li key={book.id}>
             <Book id={book.id} title={book.title} author={book.author} />
+            <button type="button" onClick={() => dispatch(removeBook(book.id))}>Remove</button>
           </li>
         ))}
       </ul>
