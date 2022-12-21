304f61888d6f24de1cf42a4009444b16d51a0619
SimonGideon <simongideon918@gmail.com>
2022-12-21 19:06:07 +0300

    ADDED stylings

--- src/Pages/Categories.jsx
+++ src/Pages/Categories.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { checkStatus } from '../redux/categories/categories';
+import '../styles/Categories.css';
 
 const Categories = () => {
   const categories = useSelector((state) => state.categories);
   const dispatch = useDispatch();
   return (
-    <div>
-      <button type="button" onClick={() => dispatch(checkStatus())}>Check Status</button>
+    <section className="categories">
+      <button type="button" className="btn-status" onClick={() => dispatch(checkStatus())}>Check Status</button>
       <h1>{categories}</h1>
-    </div>
+    </section>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,39 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../styles/Navbar.css';
+import { ImUser } from 'react-icons/im';
 
 const Navbar = () => {
   const links = [
     {
       id: 1,
       path: '/',
-      text: 'Books',
+      text: 'BOOKS',
     },
     {
       id: 2,
       path: '/categories',
-      text: 'Categories',
+      text: 'CATEGORIES',
     },
   ];
   return (
-    <nav>
-      <h1>Bookstore CMS</h1>
-      <ul>
-        {links.map((link) => (
-          <li key={link.id}>
-            <NavLink to={link.path}>
-              {link.text}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <h1>Bookstore CMS</h1>
+        <ul>
+          {links.map((link) => (
+            <li key={link.id}>
+              <NavLink to={link.path} className={({ isActive }) => (isActive ? 'activelink' : undefined)}>
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className="avatar">
+        <ImUser />
+      </div>
+    </header>
   );
 };
 
